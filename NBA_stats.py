e53f95ddf9a7b7e08bcdc8e90244a90d749eb399
Ryan <duquet.ryan@gmail.com>
2021-06-14 19:36:07 -0700

    Got the JSON to properly display data and now working on displaying more data

--- NBA_stats.py
+++ NBA_stats.py
@@ -1,11 +1,12 @@
 from plumbum import cli
 from pyfiglet import Figlet
 from plumbum.cmd import git
-from questionary import select, text
+from questionary import select, text, prompt
 import requests
 import json
 
-player_info_response = requests.get("https://www.balldontlie.io/api/v1/players")
+player_info_url = "https://www.balldontlie.io/api/v1/players"
+
 player_season_stats_response = requests.get("https://www.balldontlie.io/api/v1/season_averages")
 team_info_response = requests.get("https://www.balldontlie.io/api/v1/teams")
 
@@ -13,30 +14,52 @@ def print_welcome(text: str):
     print(Figlet(font='bulbhead').renderText(text))
 
 def generate_choices():
-    select(
+    return select(
         "What NBA info would you like to see?",
         choices=[
             "Player attributes (height, weight, etc.)",
             "Player stats",
-            "Team information"
+            "Game stats on a specific day"
         ]).ask()
 
 def generate_question_from_choice(choice: str):
-    if choice == "Player attributes":
-        answer = text("Whose player attributes are you looking for? Enter their first and last name (like Lebron James).").ask()
+    print(choice)
+    if choice == "Player attributes (height, weight, etc.)":
+        nba_player = text("Whose player attributes are you looking for? Enter their first and last name (like LeBron James).").ask()
+        print_player_attributes(nba_player)
     elif choice == "Player stats":
-        answer = text("Whose player stats are you looking for? Enter their first and last name (like Lebron James).").ask()
+        answer = text("Whose player stats are you looking for? Enter their first and last name (like LeBron James).").ask()
     else:
-        answer = text("Which team are you looking for? Enter the team's full name.").ask()
+        date = text("Which game day are you looking for? Type the date as 'YYYY-MM-DD' format.").ask()
 
 def print_player_attributes(player: str):
+    first_name = player.split()[0].strip()
+    last_name = player.split()[1].strip()
+    player_info_response = requests.get(player_info_url + "?search=" + last_name + "&per_page=1000")
+    player_info_json = player_info_response.json()
+    for person in player_info_json['data']:
+        if person['first_name'] == first_name and person['last_name'] == last_name:
+            print_formatted_attributes(person) 
 
+#def print_player_stats(player: str):  
 
-def print_formatted_json(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=False, indent=4)
-    print(text)
+#def print_team_infor(team: str):
 
+def print_formatted_attributes(obj):
+    # create a formatted string of the Python JSON object displaying the players characteristics
+    name = obj['last_name'] + ", " + obj['first_name']
+    if obj['position'] is not None:
+        position = obj['position']
+    else:
+        position = "Not found"    
+    if obj['height_feet'] is not None and obj['height_inches'] is not None:
+        height = str(obj['height_feet']) + "\'" + str(obj['height_inches']) + "\""
+    else:
+        height = "Not found"    
+    team = obj['team']['full_name'] + " (" + obj['team']['abbreviation'] + ")"
+    conference = obj['team']['conference']
+    division = obj['team']['division']
+    print("Name: " + name + "\nPosition: " + position + "\nHeight: " + height + "\nTeam: " + team + "\nConference: " + conference + "\nDivision: " + division)
 
 class GetNBAInformation(cli.Application):
     VERSION = "1.0"
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # NBA-Fancy-CLI
-A Fancy CLI that displays stats about NBA teams/players. I used this api to get data: https://www.balldontlie.io/
+A Fancy CLI that displays stats about NBA teams/players. I used the balldontlie API: https://www.balldontlie.io/ 
